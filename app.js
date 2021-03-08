f19e0978039bd1cedaa75446ec0a963ff2d877d9
rodriandrade <rodriandrade1498@gmail.com>
2021-03-07 21:07:05 -0300

    try again

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ var cors = require('cors');
 var querystring = require('querystring');
 var cookieParser = require('cookie-parser');
 
+var tokens = [];
 var client_id = '1b015d21997143e28e0724a9646dedd3'; // Your client id
 var client_secret = '732b088b97ba44ecb86a96a290f1f8e4'; // Your secret
 var redirect_uri = 'https://my-spotify-data-center-server.vercel.app/callback'; // Your redirect uri
@@ -36,8 +37,7 @@ var stateKey = 'spotify_auth_state';
 
 var app = express();
 
-//app.use(express.static(__dirname + '/public'))
-app.use(express.json())
+app.use(express.static(__dirname + '/public'))
    .use(cors())
    .use(cookieParser());
 
@@ -45,7 +45,7 @@ app.get('/login', function(req, res) {
 
   var state = generateRandomString(16);
   res.cookie(stateKey, state);
-  console.log("Hola");
+  console.log("hola");
   // your application requests authorization
   var scope = 'playlist-modify-public playlist-modify-private user-top-read user-read-recently-played user-read-currently-playing user-read-playback-state playlist-read-private playlist-read-collaborative user-follow-modify user-follow-read user-library-read user-library-modify user-modify-playback-state';
   res.redirect('https://accounts.spotify.com/authorize?' +
@@ -62,12 +62,10 @@ app.get('/callback', function(req, res) {
 
   // your application requests refresh and access tokens
   // after checking the state parameter
-  console.log(code)
-  console.log(state)
+
   var code = req.query.code || null;
   var state = req.query.state || null;
   var storedState = req.cookies ? req.cookies[stateKey] : null;
-  console.log(storedState)
 
   if (state === null || state !== storedState) {
     res.redirect('/#' +
@@ -95,12 +93,31 @@ app.get('/callback', function(req, res) {
         var access_token = body.access_token,
             refresh_token = body.refresh_token;
 
+        tokens.push(access_token);
+        tokens.push(refresh_token);
+
+        var options = {
+          url: '/tokens',
+          json: true
+        };
+
+        // use the access token to access the Spotify Web API
+        request.post(options, function(error, response, body) {
+          /*
+          res.send({
+            'access_token': access_token,
+            'refresh_token': refresh_token
+          });
+          */
+        });
+
         // we can also pass the token to the browser to make requests from there
         res.redirect('https://my-spotify-data-center.vercel.app/#' +
           querystring.stringify({
             access_token: access_token,
             refresh_token: refresh_token
           }));
+
       } else {
         res.redirect('/#' +
           querystring.stringify({
@@ -111,9 +128,15 @@ app.get('/callback', function(req, res) {
   }
 });
 
+app.get('/tokens', function(req, res) {
+  res.send(tokens);
+  console.log(tokens);
+});
+
 app.get('/refresh_token', function(req, res) {
 
   // requesting access token from refresh token
+  console.log(req.query);
   var refresh_token = req.query.refresh_token;
   var authOptions = {
     url: 'https://accounts.spotify.com/api/token',
@@ -124,10 +147,11 @@ app.get('/refresh_token', function(req, res) {
     },
     json: true
   };
- 
+
   request.post(authOptions, function(error, response, body) {
     if (!error && response.statusCode === 200) {
       var access_token = body.access_token;
+      console.log("Holiiiiiiis")
       res.send({
         'access_token': access_token
       });
